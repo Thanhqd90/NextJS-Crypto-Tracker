d2e21978b1705991f25adc67fc96e3868e4de217
Thanh Dang <thanhqd1990@gmail.com>
2021-02-19 17:17:50 -0600

    MVP

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
-import Head from "next/head";
+import { useState, useRouter } from "react";
+import CoinList from "../components/CoinList";
 import SearchBar from "../components/SearchBar";
-import Coins from "../components/Coins";
+import Layout from "../components/Layout";
 
-export default function Home() {
+export default function Home({ filteredCoins }) {
+  const [search, setSearch] = useState("");
+
+  const allCoins = filteredCoins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
+
   return (
-    <div>
-      <Head>
-        <title>Crypto Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Homepage</h1>
-
-      <SearchBar type="text" placeholder="Enter currency" />
-      <Coins />
-    </div>
+    <Layout>
+      <div className="coin_app">
+        <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
+      </div>
+    </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  );
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
